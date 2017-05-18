9edf576077fd2c58e4ccf39ee0a9f50e62b05c7c
slayer <valery.tereshko@gmail.com>
2017-05-18 13:50:39 +0300

    fixes

--- install/install.sh
+++ install/install.sh
@@ -3,5 +3,5 @@ curl https://raw.githubusercontent.com/sukaslayer/pm2-zabbix/master/install/init
 chkconfig pm2-zabbix on
 service pm2-zabbix start
 curl https://raw.githubusercontent.com/sukaslayer/pm2-zabbix/master/install/zabbix-agent/pm2-zabbix-sudoers > /etc/sudoers.d/pm2-zabbix-sudoers
-curl https://raw.githubusercontent.com/sukaslayer/pm2-zabbix/master/install/zabbix-agent/pm2-zabbix.conf > /etc/zabbix/zabbix-agentd.d/pm2-zabbix.conf
+curl https://raw.githubusercontent.com/sukaslayer/pm2-zabbix/master/install/zabbix-agent/pm2-zabbix.conf > /etc/zabbix/zabbix_agentd.d/pm2-zabbix.conf
 service zabbix-agent restart
